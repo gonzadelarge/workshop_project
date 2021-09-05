f6f62a360e025adf6b30d0e2a572fa3134e83e71
GonzaDeLarge <calvo.pellicer@bootcamp-upgrade.com>
2021-09-05 20:49:11 +0200

    FEAT mailer v1

--- src/Manager/WorkshopManager.php
+++ src/Manager/WorkshopManager.php
@@ -6,6 +6,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
 class WorkshopManager 
+
 {
 
     protected $mailer;
@@ -13,21 +14,16 @@ class WorkshopManager
 
     public function __construct( MailerInterface $mailer)
     {
-        $this->mailer-> $mailer;
+        $this->mailer = $mailer;
     }
 
     public function sendMail($subject, $body)
     {
         $email = (new Email())
-            ->from('moycarretero@gmail.com')
+            ->from('gonzadelarge@gmail.com')
             ->to('gonzapaint@gmail.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
             ->subject($subject)
             ->text($body);
-            //->html('<p>See Twig integration for better HTML integration!</p>');
 
         $this->mailer->send($email);
     }
